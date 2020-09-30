95f7728f1741eac0a72d44b34c171e212ab85f17
Kai Carver <kaicarver@gmail.com>
2020-09-30 21:13:32 +0200

    first working example

--- tutorial/tutorial/spiders/quotes_spider.py
+++ tutorial/tutorial/spiders/quotes_spider.py
@@ -15,4 +15,8 @@ def start_requests(self):
 
 
 def parse(self, response):
-    page = response.
+    page = response.url.split("/")[-2]
+    filename = 'quotes-%s.html' % page
+    with open(filename, 'wb') as f:
+        f.write(response.body)
+    self.log('Saved file %s' % filename)
