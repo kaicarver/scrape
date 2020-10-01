14d1044ccd607ca499f3251e712a9fd3e312f5b4
Kai Carver <kaicarver@gmail.com>
2020-10-01 22:29:00 +0200

    debugged amazingly buggy example

--- README.md
+++ README.md
@@ -17,6 +17,11 @@ But let's be patient and start with the tutorial.
 
 <https://docs.scrapy.org/en/latest/intro/tutorial.html>
 
-This looks like a good explanation of how to login
+This looks like a good explanation of how to login:
 
 <https://towardsdatascience.com/scrapy-this-is-how-to-successfully-login-with-ease-ea980e2c5901>
+
+Well it's nice except the code example has about 10 bugs...
+This page will also help:
+
+<https://python.gotrained.com/scrapy-formrequest-logging-in/>
--- tutorial/tutorial/spiders/login_spider.py
+++ tutorial/tutorial/spiders/login_spider.py
@@ -1,16 +1,15 @@
 import scrapy
-from scrapy.http import FormRequests
+import requests
 
 
-class LoginSpider(Scrapy.spider):
-    name = 'login'
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
     allowed_domains = ['quotes.toscrape.com']
     start_urls = ['http://quotes.toscrape.com/login']
 
+    def parse(self, response):
+        csrf_token = response.xpath('//*[@name="csrf_token"]/@value').extract_first()
+        yield scrapy.FormRequest.from_response(response, formdata={'csrf_token': csrf_token, 'user': 'user', 'pass': 'pass'}, callback=self.parse_after_login)
 
-def parse(self, response):
-        csrf_token = response.xpath('//*[@name='csrf_token']
-                     /@value').extract_first()')
-        yield FormRequests.from_response(response, formdata = {'csrf_token': csrf_token, 'user': 'user', 'pass', 'pass'}, callback = self.parse_after_login)
-def parse_after_login(self, response):
-    pass
+    def parse_after_login(self, response):
+        pass
