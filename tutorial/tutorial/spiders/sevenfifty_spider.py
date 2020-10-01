94325536f3c278521aa363fca517f940bb0f1f9c
Kai Carver <kaicarver@gmail.com>
2020-10-02 00:40:20 +0200

    success scraping 750words.com!

---
+++ tutorial/tutorial/spiders/sevenfifty_spider.py
@@ -0,0 +1,26 @@
+import scrapy
+from scrapy.http import FormRequest
+
+
+class SevenfiftySpider(scrapy.Spider):
+    name = 'sevenfifty'
+    allowed_domains = ['750words.com']
+    start_urls = ['https://750words.com/auth']
+
+    def parse(self, response):
+        authenticity_token = response.xpath(
+            '//*[@name="authenticity_token"]/@value').extract_first()
+        yield FormRequest.from_response(response,
+                                        formdata={
+                                            'authenticity_token': authenticity_token,
+                                            'person[email_address]': 'kaicarver@gmail.com',
+                                            'person[password]': 'kaS369CLVDIxQ'},
+                                        callback=self.parse_after_login)
+
+    def parse_after_login(self, response):
+        print("logged in!")
+        #print(response.body)
+        filename = 'sevenfifty.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
